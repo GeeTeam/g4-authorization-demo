34a57d546a1990bd2196d59bdabc6dfdd239abd1
xuyuhui <xuyuhui@geetest.com>
2019-01-09 15:59:08 +0800

    修改javademo中的比较方法

--- authorization.java
+++ authorization.java
@@ -22,7 +22,7 @@ class SortByStr implements Comparator {
 	public int compare(Object o1, Object o2) { 
 		String s1 = (String) o1;
 		String s2 = (String) o2;
-		return s1.compareToIgnoreCase(s2);
+		return s1.compareTo(s2);
 	}
 }
 
